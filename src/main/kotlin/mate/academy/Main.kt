9eb9987ab234936bf79ffc504a792f8ee61bd0c4
Oleksandr Kalenyk <carbonmail88@gmail.com>
2025-08-26 15:19:08 +0300

    kt-local-greeting solution

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,7 +1,22 @@
 package mate.academy
 
 fun greetUsers() {
-    // implement function
+    fun askName() : String {
+        println("Please enter a name (or press Enter to quit):")
+        return readln()
+    }
+
+    fun printGreeting(name: String) {
+        println("Hello, $name!")
+    }
+
+    while (true) {
+        val name = askName()
+        when {
+            name.isBlank() -> return
+            else -> printGreeting(name)
+        }
+    }
 }
 
 fun main() {
